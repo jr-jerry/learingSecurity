223d238257eaca7daceb5e9518481afbda5c6a1c
jr-jerry <jerryverma798@gmail.com>
2026-07-13 13:53:17 +0530

    feat: add JWT authentication filter and configure stateless session
    Implement JwtFilter to intercept requests, validate JWT tokens from the Authorization header, and set the authentication context for authenticated users. The filter bypasses the /api/v1/user/create endpoint. Update SecurityConfiguration to use stateless session management and register the JwtFilter before UsernamePasswordAuthenticationFilter.

---
+++ src/main/java/com/ducat/learingSecurity/Config/JwtFilter.java
@@ -0,0 +1,69 @@
+package com.ducat.learingSecurity.Config;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.ducat.learingSecurity.Service.CustomUserDetailService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtility jwtUtility;
+    private final CustomUserDetailService customUserDetailService;
+
+    
+
+    public JwtFilter(JwtUtility jwtUtility,
+            com.ducat.learingSecurity.Service.CustomUserDetailService customUserDetailService) {
+        this.jwtUtility = jwtUtility;
+        this.customUserDetailService = customUserDetailService;
+    }
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String path=request.getRequestURI();
+
+        if(path.startsWith("/api/v1/user/create")){
+            filterChain.doFilter(request, response);
+            return ; 
+        }
+        String authorizationHeader=request.getHeader("Authorization");
+
+        if(authorizationHeader!=null && authorizationHeader.startsWith("Bearer ")){
+            String token=authorizationHeader.substring(7);
+            String tokenUsername=jwtUtility.getUserName(token);
+
+            if(tokenUsername!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+
+                //tokenUsername-->db eess name se user hai kya ?
+             UserDetails userDetails=customUserDetailService.loadUserByUsername(tokenUsername);
+             String dbUserName=userDetails.getUsername();
+                //if user exist in db -->then validate jwt token with that userName
+                if(jwtUtility.validToken(token, dbUserName)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=new UsernamePasswordAuthenticationToken(userDetails,userDetails.getPassword(),List.of());
+
+                    usernamePasswordAuthenticationToken.setDetails(request.getRemoteAddr());
+                    
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+                
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/ducat/learingSecurity/Config/SecurityConfiguration.java
+++ src/main/java/com/ducat/learingSecurity/Config/SecurityConfiguration.java
@@ -10,21 +10,26 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
  
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
     private final UserDetailsService customUserDetailService;
+    private final JwtFilter jwtFilter;
 
-    public SecurityConfiguration(UserDetailsService userDetailsService){
-        this.customUserDetailService=userDetailsService;
-    }
     
+    
+    public SecurityConfiguration(UserDetailsService customUserDetailService, JwtFilter jwtFilter) {
+        this.customUserDetailService = customUserDetailService;
+        this.jwtFilter = jwtFilter;
+    }
     @Bean
     public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception{
         return http
@@ -35,7 +40,8 @@ public class SecurityConfiguration {
                     .requestMatchers(HttpMethod.POST,"/api/v1/user/create").permitAll()
                       .anyRequest().authenticated()
         )
-        .httpBasic(Customizer.withDefaults())
+        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class)
         .build();
     } 
     @Bean
